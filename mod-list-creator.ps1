fabfbfdb72be134066ef09465b9cf67d373038cb
TheOtherAnxxity <76730034+TheOtherAnxxity@users.noreply.github.com>
2022-05-23 18:22:53 +0530

    Add overworld map for new server
    Also update mod list creation script

--- mod-list-creator.ps1
+++ mod-list-creator.ps1
@@ -1,43 +1,86 @@
 # Input parameters
 param (
     [Parameter(Mandatory=$true)]
-    [string]$ModPackFolder,
-
-    [Parameter(Mandatory=$true)]
-    [string]$ModPackName,
-
-    [Parameter(Mandatory=$true)]
-    [string]$ModPackVersion,
-
+    [string]$ModPackPath,
+    
+    [Parameter(Mandatory = $true)]
+    [string]$StyleSheet,
+    
     [Parameter()]
-    [string]$ModPackSearchPath = "C:\Users\ASUS\WebstormProjects\theotheranxxity.github.io\mod-packs"
+    [string]$FontAwesome = $null
 )
 
+if (-not ($FontAwesome -eq $null)) {
+    $FontAwesome = "<script src=`"$FontAwesome`" crossorigin=`"anonymous`"></script>"
+} else {
+    $FontAwesome = ""
+}
+
 # Declare HTML used to beautify mod list
-[string]$FontAwesome = "https://kit.fontawesome.com/1565b8f9f4.js"
 [string[]]$Html = @(
     '<!DOCTYPE html>'
     '<html lang="en">'
     '<head>'
     '<title>Mod List</title>'
     '<meta charset="UTF-8">'
-    '<link rel="stylesheet" href="../../mod-list.css">'
-    "<script src=`"$FontAwesome`" crossorigin=`"anonymous`">"
-    '</script>'
+    "<link rel=`"stylesheet`" href=`"$StyleSheet`">"
+    "$FontAwesome"
     '</head>'
     '<body>'
     '</body>'
     '</html>'
 )
 
-# Construct mod pack file and path
-[string]$ModPackFile = "./$ModPackName-$ModPackVersion.zip"
-[string]$ModPackPath = Join-Path -Path $ModPackSearchPath -ChildPath $ModPackFolder
+# Validate that path exists
+if (-not (Test-Path -Path $ModPackPath)) {
+    Write-Error "Failed to validate given path"
+    Exit 1
+}
 
 # Set location
 Set-Location $ModPackPath
+
+[string]$ModPackName
+[string]$ModPackVersion
+
+# Validate that it is indeed a packwiz mod pack by running packwiz refrsh
+$PackwizRefresh = Start-Process -FilePath "packwiz" -ArgumentList "refresh" -NoNewWindow -PassThru -Wait
+if ($PackwizRefresh.ExitCode -ne 0) {
+    Write-Error "Failed to validate folder as packwiz mod pack"
+    Exit $PackwizRefresh.ExitCode
+}
+
+# Auto fetch both mod pack name and version
+foreach($line in ((Get-Content -Path "./pack.toml") | ForEach-Object -Process { $_ = $_.Trim(); $_.StartsWith("name") -or $_.StartsWith("version") })) {
+    # Yes this is pretty horrible but since there is only one name and one version key this should work.
+    [string]$Type
+    if ($line.StartsWith("name")) {
+        $line = $line.TrimStart("name")
+        $Type = "name"
+    } elseif ($line.StartsWith("version")) {
+        $line = $line.TrimStart("version")
+        $Type = "version"
+    }
+    $line = $line.TrimStart()
+    $line = $line.TrimStart('=')
+    $line = $line.Trim()
+    if ($line.StartsWith('"')) {
+        $line = $line.Trim('"')
+    } elseif ($line.StartsWith("'")) {
+        $line = $line.Trim("'")
+    }
+    if ($Type -eq "name") {
+        $ModPackName = $line
+    } elseif ($Type -eq "version") {
+        $ModPackVersion = $line
+    }
+}
+
+# Construct mod pack file and path
+[string]$ModPackFile = "./$ModPackName-$ModPackVersion.zip"
+
 # Export using packwiz
-Start-Process -FilePath "./packwiz.exe" -ArgumentList @("curseforge", "export") -NoNewWindow -Wait
+Start-Process -FilePath "packwiz" -ArgumentList @("curseforge", "export") -NoNewWindow -Wait
 
 # Create a new temp directory
 New-Item -Path "./temp" -ItemType Directory
@@ -54,10 +97,10 @@ Remove-Item $ModPackFile
 New-Item -Path "./mod-list.html" -Force
 $ModList = Get-Content -Path "./modlist.html"
 
+# Delete old modlist
+Remove-Item "./modlist.html"
+
 # Beautify old mod list and write it to new mod list
 Add-Content -Path "./mod-list.html" -Value $Html[0..9]
 Add-Content -Path "./mod-list.html" -Value $ModList
 Add-Content -Path "./mod-list.html" -Value $Html[10, 11]
-
-# Delete old modlist
-Remove-Item "./modlist.html"
